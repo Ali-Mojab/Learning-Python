7a9d5e63e2ecfb4319c1e78ab1170a89616b5d1d
Sam <its.ali.mojab@gmail.com>
2021-09-12 21:55:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Fundamental Data Types
+#integers: (int): AND #float
+print(2+4)
+print(8-10)
+print(2*8)
+print(2/4)
+#----------
+#what kind of Data Type is this?: (type()) :
+print(type(2+4))
+print(type(8-10))
+print(type(2*8))
+print(type(2/4))
+print(type(0))
+#----------
